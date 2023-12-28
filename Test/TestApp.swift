f4ccae9442cc2a2083f52b48f74a73f7d88956ba
Ibrahim <ibrahimazzam@live.com>
2023-12-28 10:53:18 +0000

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Ibrahim Azzam on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
